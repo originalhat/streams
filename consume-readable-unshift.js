cf6972cdc46fb6d9d10a48ff5273d339ca023cb4
Devin Brown <devin.g.brown@gmail.com>
2018-03-18 09:34:45 -0600

    create a readable parser that splits on lines

---
+++ consume-readable-unshift.js
@@ -0,0 +1,20 @@
+var offset = 0;
+
+process.stdin.on('readable', function () {
+    var buf = process.stdin.read();
+    if (!buf) return;
+    for (; offset < buf.length; offset++) {
+        if (buf[offset] === 0x0a) {
+            console.dir(buf.slice(0, offset).toString());
+            buf = buf.slice(offset + 1);
+            offset = 0;
+            process.stdin.unshift(buf);
+            return;
+        }
+    }
+    process.stdin.unshift(buf);
+});
+
+// tail -n +1300 /usr/share/dict/propernames | head -n10 | node consume-readable-unshift.js
+// better alterative than rolling your own: https://www.npmjs.com/package/split
+
